11fa9acde3da7c668e596916436ed707bf651375
david hall <davideltonhall@gmail.com>
2015-04-08 16:35:50 -0700

    finished chapter 10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407231035) do
+ActiveRecord::Schema.define(version: 20150408223026) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150407231035) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["account_type"], name: "index_users_on_account_type"
