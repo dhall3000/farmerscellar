d57cbe8cacff695f5421c4f66927c23b3b2b2002
david hall <davideltonhall@gmail.com>
2015-04-24 10:51:55 -0700

    farmer can now fill out a form about his posting and the data persists through to the database

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -6,4 +6,30 @@ class PostingsController < ApplicationController
   	@unit_categories = UnitCategory.all
   	@unit_kinds = UnitKind.all
   end
+
+  def create  	
+  	@posting = Posting.new(posting_params)
+  	if @posting.save
+  	  flash[:info] = "Your new posting is now live!"
+      redirect_to root_url
+    else
+      render 'new'
+  	end
+
+  end
+
+  private
+
+    def posting_params
+      
+      posting = params.require(:posting).permit(:description, :quantity_available, :price, :user_id, :product_id, :unit_category_id, :unit_kind_id)
+      posting[:user_id] = current_user[:id]
+
+      unit_kind = UnitKind.all.find_by(id: posting[:unit_kind_id])
+      posting[:unit_category_id] = unit_kind.unit_category.id
+
+      posting
+
+    end
+
 end
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,4 +3,7 @@ class Posting < ActiveRecord::Base
   belongs_to :product
   belongs_to :unit_category
   belongs_to :unit_kind
+
+  validates :quantity_available, presence: true, numericality: { only_integer: true }
+
 end
