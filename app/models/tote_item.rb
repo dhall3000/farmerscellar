d64d1758f6c7509d57cffde8828b93cdefecf03b
david hall <davideltonhall@gmail.com>
2015-05-06 11:20:40 -0700

    additional tweaks to fix small breaks

--- app/models/tote_item.rb
+++ app/models/tote_item.rb
@@ -3,7 +3,7 @@ class ToteItem < ActiveRecord::Base
   belongs_to :user
 
   def self.states
-  	{START: 0, PREAUTHORIZED: 1, COMMITTED: 2, FILLED: 3, NOTFILLED: 4}
+  	{ADDED: 0, AUTHORIZED: 1, COMMITTED: 2, FILLED: 3, NOTFILLED: 4, REMOVED: 5, PURCHASED: 6}
   end
   
 end
