fa9856d8bee9c4110d4187b70136add0ead44cf9
David Hall <david@farmerscellar.com>
2015-09-29 16:20:57 -0700

    refactoring the bulkpurchase test and adding fail purchases scenarios

--- test/integration/bulk_purchases_test.rb
+++ test/integration/bulk_purchases_test.rb
@@ -6,53 +6,68 @@ class BulkPurchasesTest < BulkBuyer
   #   assert true
   # end
 
-  test "do bulk buy" do
-    fill_all_tote_items = true
-  	create_bulk_buy(fill_all_tote_items)
-  	get new_bulk_purchase_path
-  	assert :success
-    assert_template 'bulk_purchases/new'
-    #puts @response.body
-    bulk_purchase = assigns(:bulk_purchase)
-    assert_not_nil bulk_purchase
+  #bundle exec rake test test/integration/bulk_purchases_test.rb
+  test "do bulk buy with purchase failures" do
+    purchase_receivables = setup_bulk_purchase            
+    assert_equal 4, purchase_receivables.count
+    assert_equal 0, PaymentPayable.count
+    FakeCaptureResponse.toggle_success = true
+    post bulk_purchases_path, purchase_receivables: purchase_receivables
+    assert :success
+    assert_template 'bulk_purchases/create'
+    purchase_receivables = assigns(:purchase_receivables)
+    assert_not_nil purchase_receivables
+    assert purchase_receivables.count > 0
+    puts "number of purchase receivables: #{purchase_receivables.count}"
 
-    #assert there are some purchase receivables
-    assert_not_nil bulk_purchase.purchase_receivables
-    assert bulk_purchase.purchase_receivables.to_a.count > 0
-    puts "bulk_purchase.purchase_receivables.to_a.count: #{bulk_purchase.purchase_receivables.to_a.count}"
+    #assert total amount > total amount paid    
 
-    #verify that all the pr's are legit
-    for purchase_receivable in bulk_purchase.purchase_receivables
-      assert purchase_receivable.amount > 0
-      assert_not_nil purchase_receivable.bulk_buys
-      assert purchase_receivable.bulk_buys.count > 0
+    total_purchased = 0
+    total_failed_purchases = 0
 
-      puts "purchase_receivable.bulk_buys.count: #{purchase_receivable.bulk_buys.count}"
-      
-      assert_not_nil purchase_receivable.users
-      assert purchase_receivable.users.count > 0
-
-      puts "purchase_receivable.users.count: #{purchase_receivable.users.count}"
-
-      assert_not_nil purchase_receivable.tote_items
-      assert purchase_receivable.tote_items.count > 0
-
-      puts "purchase_receivable.tote_items.count: #{purchase_receivable.tote_items.count}"
-
-      #the filled tote items should all be marked as PURCHASEPENDING by now
-      for tote_item in purchase_receivable.tote_items
-        assert_equal tote_item.status, ToteItem.states[:PURCHASEPENDING]
+    for purchase in Purchase.all
+      if purchase.response.success?
+        total_purchased += purchase.gross_amount
+      else
+        total_failed_purchases += purchase.gross_amount
       end
-
-    end
-    
-    purchase_receivables = []
-
-    #build up an array of purchase_receivable ids to simulate the post to create a bulk purchase
-    for pr in bulk_purchase.purchase_receivables
-      purchase_receivables << pr.id
     end
 
+    total_amount = 0
+    total_amount_paid = 0
+
+    #verify toteitems are updated to the proper value
+    #verify pr kind is appropriate    
+    for pr in purchase_receivables
+      total_amount += pr.amount
+      total_amount_paid += pr.amount_paid
+
+      for tote_item in pr.tote_items
+        if pr.kind == PurchaseReceivable.kind[:NORMAL]
+          assert_equal tote_item.status, ToteItem.states[:PURCHASED]          
+        else
+          if pr.kind == PurchaseReceivable.kind[:PURCHASEFAILED]
+            assert_equal tote_item.status, ToteItem.states[:PURCHASEFAILED]
+          end
+        end
+      end
+    end
+
+    total_failed_purchases2 = total_amount - total_amount_paid
+
+    assert_equal total_failed_purchases, total_failed_purchases2
+    assert total_amount_paid < total_amount
+    assert_equal total_purchased, total_amount_paid
+
+    #purchase_receivables = setup_bulk_purchase    
+
+  end
+
+  #rake test test/integration/bulk_purchases_test.rb
+  #test "do bulk buy" do
+  def skip
+    return
+    purchase_receivables = setup_bulk_purchase
     assert_equal 0, PaymentPayable.count
     post bulk_purchases_path, purchase_receivables: purchase_receivables
     assert :success
@@ -153,4 +168,57 @@ class BulkPurchasesTest < BulkBuyer
     #end
 
   end
+
+  def setup_bulk_purchase
+
+    fill_all_tote_items = true
+    create_bulk_buy(fill_all_tote_items)
+    get new_bulk_purchase_path
+    assert :success
+    assert_template 'bulk_purchases/new'
+    #puts @response.body
+    bulk_purchase = assigns(:bulk_purchase)
+    assert_not_nil bulk_purchase
+
+    #assert there are some purchase receivables
+    assert_not_nil bulk_purchase.purchase_receivables
+    assert bulk_purchase.purchase_receivables.to_a.count > 0
+    puts "bulk_purchase.purchase_receivables.to_a.count: #{bulk_purchase.purchase_receivables.to_a.count}"
+
+    #verify that all the pr's are legit
+    for purchase_receivable in bulk_purchase.purchase_receivables
+      assert purchase_receivable.amount > 0
+      assert_not_nil purchase_receivable.bulk_buys
+      assert purchase_receivable.bulk_buys.count > 0
+
+      puts "purchase_receivable.bulk_buys.count: #{purchase_receivable.bulk_buys.count}"
+      
+      assert_not_nil purchase_receivable.users
+      assert purchase_receivable.users.count > 0
+
+      puts "purchase_receivable.users.count: #{purchase_receivable.users.count}"
+
+      assert_not_nil purchase_receivable.tote_items
+      assert purchase_receivable.tote_items.count > 0
+
+      puts "purchase_receivable.tote_items.count: #{purchase_receivable.tote_items.count}"
+
+      #the filled tote items should all be marked as PURCHASEPENDING by now
+      for tote_item in purchase_receivable.tote_items
+        assert_equal tote_item.status, ToteItem.states[:PURCHASEPENDING]
+      end
+
+    end
+    
+    purchase_receivables = []
+
+    #build up an array of purchase_receivable ids to simulate the post to create a bulk purchase
+    for pr in bulk_purchase.purchase_receivables
+      purchase_receivables << pr.id
+    end    
+
+    return purchase_receivables
+
+  end
+
 end
