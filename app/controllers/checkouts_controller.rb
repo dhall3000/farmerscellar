82bc12c0f0e73749361adc46df5703881c0620d2
David Hall <david@farmerscellar.com>
2016-01-14 17:31:49 -0800

    order summary details now show up on paypal's checkout page when user goes through checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -8,10 +8,11 @@ class CheckoutsController < ApplicationController
       return
     end
 
-    if USEGATEWAY
+    if USEGATEWAY      
       response = GATEWAY.setup_authorization(
         params[:amount].to_f * 100,
         ip: request.remote_ip,
+        items: get_order_summary_details_for_paypal_display(@unauthorized_tote_items),
         return_url: new_authorization_url,
         cancel_return_url: root_url,
         allow_guest_checkout: true
@@ -37,6 +38,42 @@ class CheckoutsController < ApplicationController
       flash[:danger] = "Payment checkout error."
   	end    
   end
+
+  private
+    def get_order_summary_details_for_paypal_display(tote_items)
+      summary_items = []
+
+      if tote_items == nil || !tote_items.any?
+        return summary_items
+      end
+
+      tote_items.each do |tote_item|
+        summary_item = get_order_summary_item(tote_item)
+        summary_items << summary_item
+      end
+
+      return summary_items
+ 
+    end
+
+    def get_order_summary_item(tote_item)
+      summary_item = {}
+
+      if tote_item == nil
+        return summary_item
+      end
+
+      name = tote_item.posting.product.name
+      description = "Producer - " + tote_item.posting.user.name
+
+      summary_item[:name] = name
+      summary_item[:description] = description
+      summary_item[:quantity] = tote_item.quantity
+      summary_item[:amount] = (tote_item.price).round(2) * 100
+
+      return summary_item
+      
+    end
 end
 
 class FakeCheckoutResponse
