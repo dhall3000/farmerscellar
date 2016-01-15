10417ac64718bdcd1533e14a0df7aef55664867f
David Hall <david@farmerscellar.com>
2016-01-15 11:58:58 -0800

    added test for authorization receipt sending

--- test/authorization_helper.rb
+++ test/authorization_helper.rb
@@ -59,17 +59,43 @@ class Authorizer < ActionDispatch::IntegrationTest
     puts "checkout amount: #{checkout.amount}"
     assert_redirected_to new_authorization_path(token: checkout.token)    
     follow_redirect!    
-    authorization = assigns(:authorization)
+    authorization = assigns(:authorization)    
     assert_not_nil authorization
     assert authorization.token = checkout.token, "authorization.token not equal to checkout.token"
     assert authorization.amount = checkout.amount, "authorization.amount not equal to checkout.token"
     assert_template 'authorizations/new'
-    post authorizations_path, authorization: {token: authorization.token, payer_id: authorization.payer_id, amount: authorization.amount}
+    num_mail_messages_sent = ActionMailer::Base.deliveries.size
+    post authorizations_path, authorization: {token: authorization.token, payer_id: authorization.payer_id, amount: authorization.amount}    
     authorization = assigns(:authorization)
+    verify_authorization_receipt_sent(num_mail_messages_sent, customer, authorization)
     assert_not_nil authorization
     assert_not_nil authorization.transaction_id
     assert_template 'authorizations/create'
     return authorization
+  end
+
+  def verify_authorization_receipt_sent(num_mail_messages_sent, user, authorization)
+
+    #did a mail message even go out?
+    assert_equal ActionMailer::Base.deliveries.size, num_mail_messages_sent + 1
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal [user.email], mail.to
+    assert_match "Payment authorization receipt", mail.subject    
+    assert_match "payment authorization receipt for", mail.body.encoded
+    
+    assert_match authorization.checkouts.last.tote_items.last.posting.user.farm_name, mail.body.encoded
+    assert_match authorization.checkouts.last.tote_items.last.posting.product.name, mail.body.encoded
+    assert_match authorization.checkouts.last.tote_items.last.price.to_s, mail.body.encoded
+    assert authorization.checkouts.last.tote_items.last.price > 0
+    assert authorization.checkouts.last.tote_items.last.quantity > 0
+    assert_match authorization.checkouts.last.tote_items.last.quantity.to_s, mail.body.encoded
+    assert_match authorization.checkouts.last.tote_items.last.posting.unit_kind.name, mail.body.encoded
+    assert_match authorization.checkouts.last.tote_items.last.posting.delivery_date.strftime("%A %b %d, %Y"), mail.body.encoded
+
+    assert authorization.amount > 0
+    assert_match authorization.amount.to_s, mail.body.encoded
+
   end
 
 end
