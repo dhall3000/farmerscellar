142879b43617d7eaa4b254af855efb0eb17df085
David Hall <david@farmerscellar.com>
2016-02-03 18:16:35 -0800

    displayed dropsites deliverable on delivery new view

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -7,12 +7,25 @@ class DeliveriesController < ApplicationController
     #-don't have any delivery objects associated
     postings2 = postings1.includes(:delivery_postings).where( delivery_postings: { posting_id: nil } )
     #-have tote_items in any of states FILLED, PURCHASEPENDING, PURCHASED or PURCHASEFAILED
-    @delivery_eligible_postings = postings2.includes(:tote_items).where( tote_items: {status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASEPENDING], ToteItem.states[:PURCHASED], ToteItem.states[:PURCHASEFAILED]]})
-
-    
-
+    @delivery_eligible_postings = postings2.includes(:tote_items).where( tote_items: {status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASEPENDING], ToteItem.states[:PURCHASED], ToteItem.states[:PURCHASEFAILED]]})    
 
     #get dropsites that must be delivered to for this set of postings
+    user_ids = []
+
+    @delivery_eligible_postings.each do |posting|
+      posting.tote_items.each do |ti|
+        user_ids << ti.user.id
+      end
+    end
+
+    users = User.find(user_ids.uniq)
+    dropsite_ids = []
+
+    users.each do |user|
+      dropsite_ids = user.dropsites.last.id
+    end
+
+    @dropsites = Array(Dropsite.find(dropsite_ids))
 
   end
 
