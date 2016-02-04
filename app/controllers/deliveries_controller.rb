9134c8a9ad82fb5f10d979cfd488f32bcb194452
David Hall <david@farmerscellar.com>
2016-02-04 15:47:54 -0800

    added logic for deliveries  show, edit and update commands

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,31 +1,28 @@
 class DeliveriesController < ApplicationController
+
   def new
-    #get postings that have no deliveries and whose delivery date is before tomorrow and that have toteitems in a deliverable state
+
+    prod_mode = true
     
-    #-delivery date is before tomorrow
-    postings1 = Posting.where("delivery_date < ?", Date.today + 1)
-    #-don't have any delivery objects associated
-    postings2 = postings1.includes(:delivery_postings).where( delivery_postings: { posting_id: nil } )
-    #-have tote_items in any of states FILLED, PURCHASEPENDING, PURCHASED or PURCHASEFAILED
-    @delivery_eligible_postings = postings2.includes(:tote_items).where( tote_items: {status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASEPENDING], ToteItem.states[:PURCHASED], ToteItem.states[:PURCHASEFAILED]]})    
+    if prod_mode
+      #get postings that have no deliveries and whose delivery date is before tomorrow and that have toteitems in a deliverable state
+    
+      #-delivery date is before tomorrow
+      postings1 = Posting.where("delivery_date < ?", Date.today + 1)
+      #-don't have any delivery objects associated
+      postings2 = postings1.includes(:delivery_postings).where( delivery_postings: { posting_id: nil } )
+      #-have tote_items in any of states FILLED, PURCHASEPENDING, PURCHASED or PURCHASEFAILED
+      @delivery_eligible_postings = postings2.includes(:tote_items).where( tote_items: {status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASEPENDING], ToteItem.states[:PURCHASED], ToteItem.states[:PURCHASEFAILED]]})    
 
-    #get dropsites that must be delivered to for this set of postings
-    user_ids = []
-
-    @delivery_eligible_postings.each do |posting|
-      posting.tote_items.each do |ti|
-        user_ids << ti.user.id
-      end
+      #get dropsites that must be delivered to for this set of postings
+      @dropsites = get_dropsites_from_postings(@delivery_eligible_postings)      
+    else
+      #the purpose of this is for developing the deliveries features. the prod code creates a new delivery only with postings that have elsewhere been marked as delivered.
+      #this here dev code will allow you to just keep using the same postings in the creation of new deliveries so that you don't have to repeatedly reseed the db      
+      postings1 = Posting.where("delivery_date < ?", Date.today + 1)
+      @delivery_eligible_postings = postings1.includes(:tote_items).where( tote_items: {status: get_tote_item_states})    
+      @dropsites = get_dropsites_from_postings(@delivery_eligible_postings)
     end
-
-    users = User.find(user_ids.uniq)
-    dropsite_ids = []
-
-    users.each do |user|
-      dropsite_ids = user.dropsites.last.id
-    end
-
-    @dropsites = Array(Dropsite.find(dropsite_ids))
 
   end
 
@@ -38,12 +35,24 @@ class DeliveriesController < ApplicationController
       delivery.postings << posting
     end
 
+    flash[:success] = "New delivery created."
+    redirect_to delivery_path(delivery)
+
   end
 
   def edit
+    @delivery = Delivery.find(params[:id])
+    @dropsites_deliverable = get_dropsites_from_postings(@delivery.postings)
   end
 
   def update
+    @delivery = Delivery.find(params[:id])
+    dropsite = Dropsite.find(params[:dropsite_id])
+    @delivery.dropsites << dropsite
+
+    @dropsites_deliverable = get_dropsites_from_postings(@delivery.postings)
+    flash.now[:success] = dropsite.name + " delivery saved and delivery notifications sent."
+    render 'show'
   end
 
   def index
@@ -51,14 +60,45 @@ class DeliveriesController < ApplicationController
   end
 
   def show
+    @delivery = Delivery.find(params[:id])
+    @dropsites_deliverable = get_dropsites_from_postings(@delivery.postings)
   end
 
   def destroy
   end
 
   private
+    
     def delivery_params
       params.require(:posting_ids)
+    end
+
+    def get_tote_item_states
+      return [ToteItem.states[:FILLED], ToteItem.states[:PURCHASEPENDING], ToteItem.states[:PURCHASED], ToteItem.states[:PURCHASEFAILED]]
+    end
+
+    #returns an array of dropsites from the given postings
+    def get_dropsites_from_postings(postings)
+
+      user_ids = []
+
+      postings.each do |posting|        
+        posting.tote_items.where(status: get_tote_item_states).each do |ti|
+          user_ids << ti.user.id
+        end
+      end
+
+      users = User.find(user_ids.uniq)
+      dropsite_ids = []
+
+      users.each do |user|
+        dropsite_ids << user.dropsites.last.id
+      end
+
+      dropsites = Array(Dropsite.find(dropsite_ids.uniq))
+
+      return dropsites
+
     end
 
 end
