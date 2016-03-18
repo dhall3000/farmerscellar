839b91bd03f034586205f06db30525817167c040
David Hall <david@farmerscellar.com>
2016-03-18 10:50:36 -0700

    moved bulk buy create action to FundsProcessing class

--- app/controllers/bulk_buys_controller.rb
+++ app/controllers/bulk_buys_controller.rb
@@ -14,59 +14,8 @@ class BulkBuysController < ApplicationController
   end
 
   def create
-
-    #create a bulkbuy object
-    @bulk_buy = BulkBuy.new
-    @bulk_buy.admins << current_user
-    @bulk_buy.amount = 0;
-
-    authorizations = {}
-
-    #get the filled tote items and group them by user's authorization
-  	filled_tote_items = ToteItem.where(id: params[:filled_tote_item_ids])
-    
-    if filled_tote_items == nil || filled_tote_items.count < 1
-      return
-    end
-
-  	for filled_tote_item in filled_tote_items
-      @bulk_buy.tote_items << filled_tote_item
-  	  if filled_tote_item.checkouts != nil && filled_tote_item.checkouts.any?
-  	  	if filled_tote_item.checkouts.last.authorizations != nil && filled_tote_item.checkouts.last.authorizations.any?
-  	  	  authorization = filled_tote_item.checkouts.last.authorizations.last
-  	  	  if authorizations[authorization.token] == nil
-  	  	  	authorizations[authorization.token] = {amount: 0, authorization: authorization, filled_tote_items: []}
-  	  	  end
-  	  	  authorizations[authorization.token][:amount] = (authorizations[authorization.token][:amount] + (filled_tote_item.quantity * filled_tote_item.price).round(2)).round(2)
-  	  	  authorizations[authorization.token][:filled_tote_items] << filled_tote_item
-  	  	end
-  	  end
-  	end
-
-    #authorizations[token][:amount] //this is the purchase_receivable amount
-    #authorizations[token][:authorization]
-    #authorizations[token][:filled_tote_items]
-
-    authorizations.each do |token, value|
-      ftis = value[:filled_tote_items]
-      if ftis == nil || !ftis.any?
-        next
-      end
-
-      @bulk_buy.purchase_receivables.build(amount: value[:amount], amount_purchased: 0, kind: PurchaseReceivable.kind[:NORMAL])
-      pr = @bulk_buy.purchase_receivables.last
-      user = User.find(ftis.first.user_id)                      
-      pr.users << user
-
-      ftis.each do |fti|
-        pr.tote_items << fti
-        fti.update(status: ToteItem.states[:PURCHASEPENDING])
-      end                              
-
-      #this represents the total value of everything that was filled for this bulk buy
-      @bulk_buy.amount = (@bulk_buy.amount + value[:amount]).round(2)
-    end
-  	#save the bulkbuy object
-  	@bulk_buy.save
+    values = FundsProcessing.bulk_buy_create(params[:filled_tote_item_ids], current_user)
+    @bulk_buy = values[:bulk_buy]    
   end
+  
 end
