022269c1f2dd290420f54103c12ede1adc110d83
David Hall <david@farmerscellar.com>
2016-03-24 16:11:34 -0700

    oops, broke popovers for the shopping page with the last commit

--- app/assets/javascripts/postings.js
+++ app/assets/javascripts/postings.js
@@ -2,4 +2,5 @@ $(function()
 { 
   $('#dtpDeliveryDate').datetimepicker({daysOfWeekDisabled: [0], format: "dddd, MMMM Do YYYY", ignoreReadonly: true}); 
   $('#dtpCommitmentZoneStart').datetimepicker({stepping: 5, format: "dddd, MMMM Do YYYY, h:mm A", ignoreReadonly: true});
+  $(function(){ $('.btn').popover(); });
 });
