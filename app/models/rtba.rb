154e38fd5a8d7a17cfac3e2e564414ab394a7fb6
David Hall <david@farmerscellar.com>
2016-04-01 10:12:17 -0700

    added model validations and tests for rtba

--- app/models/rtba.rb
+++ app/models/rtba.rb
@@ -1,4 +1,8 @@
 class Rtba < ActiveRecord::Base
   belongs_to :user
   has_many :rtauthorizations
+
+  validates_presence_of :user
+  validates :token, :ba_id, presence: true
+
 end
--- test/models/rtba_test.rb
+++ test/models/rtba_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class RtbaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:c1)
+		@rtba = Rtba.new(token: "faketoken", ba_id: "fake_ba_id", user_id: @user.id, active: true)
+	end
+
+	test "should not save without token" do
+		@rtba.token = nil
+		assert_not @rtba.save
+		assert_not @rtba.valid?		
+	end
+
+	test "should not save without ba_id" do
+		@rtba.ba_id = nil
+		assert_not @rtba.save
+		assert_not @rtba.valid?		
+	end
+
+	test "should not save without user" do
+		@rtba.user_id = nil
+		assert_not @rtba.save
+		assert_not @rtba.valid?		
+	end
+
 end
