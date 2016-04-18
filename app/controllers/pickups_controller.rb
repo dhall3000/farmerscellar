36431bbd34a64d419160a34f701a6a64032c1391
David Hall <david@farmerscellar.com>
2016-04-18 10:46:33 -0700

    added logic to pickup controller to select proper toteitems for display. it's not tested though. that's what i'm about to work on...

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -18,6 +18,27 @@ class PickupsController < ApplicationController
 	  		render 'pickups/new'	  	
   		else
   			@user = @pickup_code.user
+  			#get a product list of everything that's been delivered since the last pickup (or 7 days, whichever is more recent)
+  			#TODO: for now for 'delivered' we're going to use toteitem states FILLED or PURCHASED
+  			#this will eventually change though once we overhaul the toteitem statemachine
+  			
+  			#A) 7 days ago
+  			last_pickup = 7.days.ago
+  			if @user.pickups.any?
+  				#or...
+  				#B) the last pickup
+  				last_pickup = @user.pickups.last.created_at
+  			end
+
+  			#whichever is more recent
+  			cutoff = [last_pickup, 7.days.ago].max
+  			#@tote_items = @user.tote_items.where(status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASED]], "updated_at > ?", last_pickup)
+  			#TODO: the below line isn't quite right. it will display toteitems that have been purchased in the last 7 days even though it could be that
+  			#some purchased items were actually delivered a longer time period ago like, say, 9 days ago. the reason is because the .updated_at field
+  			#will get modified when the purchas goes through. leaving it this way cause i'll be redoing it anyway after cleaning up the toteitem state machine
+  			@tote_items = @user.tote_items.where(status: [ToteItem.states[:FILLED], ToteItem.states[:PURCHASED]]).where("updated_at > ?", last_pickup)
+  			#now create a new pickup to represent the current pickup  				 				
+ 				@user.pickups.create
   		end
   	else
   		flash.now[:danger] = "Invalid code entry"
--- test/controllers/pickups_controller_test.rb
+++ test/controllers/pickups_controller_test.rb
@@ -45,32 +45,41 @@ class PickupsControllerTest < ActionController::TestCase
 
   test "should create" do
   	log_in_as(@dropsite_user)
-    post :create, pickup_code: "1234"
+  	pickups_count = Pickup.count
+  	c1 = users(:c1)
+    post :create, pickup_code: c1.pickup_code.code
     assert_response :success
     pickup_code = assigns(:pickup_code)
     assert pickup_code.valid?
     user = assigns(:user)
     assert user.valid?
+    #a new pickup object should have been created as a result of this pickup action
+    assert_equal pickups_count + 1, Pickup.count
+    assert_equal c1.id, Pickup.all.last.user_id
   end
 
   test "should not create when invalid code submitted" do
   	log_in_as(@dropsite_user)
+  	pickups_count = Pickup.count
     post :create, pickup_code: "12A45"
     assert_response :success
     pickup_code = assigns(:pickup_code)
     assert_not pickup_code.valid?
     assert_not flash.empty?
     assert_equal "Invalid code entry", flash[:danger]
+    assert_equal pickups_count, Pickup.count
   end
 
   test "should not create when non existent code submitted" do
   	log_in_as(@dropsite_user)
+  	pickups_count = Pickup.count
     post :create, pickup_code: "1345"
     assert_response :success
     pickup_code = assigns(:pickup_code)
     assert pickup_code.nil?
     assert_not flash.empty?
     assert_equal "Invalid code entry", flash[:danger]
+    assert_equal pickups_count, Pickup.count
   end
 
 end
