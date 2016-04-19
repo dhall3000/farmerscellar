a4b2a571a0533962d2465b0b610f9f038bb3959d
David Hall <david@farmerscellar.com>
2016-04-19 08:52:36 -0700

    blocked DROPSITE users from accessing anything in the app besides PickupsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base  
   include SessionsHelper, ToteItemsHelper
 
+  before_action :redirect_dropsite_user
   before_filter :set_cache_headers
 
   # Prevent CSRF attacks by raising an exception.
@@ -13,9 +14,21 @@ class ApplicationController < ActionController::Base
     redirect_to login_path    
   end
 
-  def set_cache_headers
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-  end
+  private
+
+    def set_cache_headers
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
+
+    #we want to block dropsite users for all app functionality except PickupsController
+    def redirect_dropsite_user      
+      if logged_in? && current_user.account_type_is?(:DROPSITE)
+        if self.class.to_s != "PickupsController"
+          redirect_to(new_pickup_url)
+        end
+      end
+    end
+
 end
