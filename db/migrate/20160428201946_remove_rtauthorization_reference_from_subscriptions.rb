fd995e3bae4c35d09ef1c2bcbc0eb55fe0d36c7f
David Hall <david@farmerscellar.com>
2016-05-07 14:01:44 -0700

    fixed a typo in a migration file. bummer.

--- db/migrate/20160428201946_remove_rtauthorization_reference_from_subscriptions.rb
+++ db/migrate/20160428201946_remove_rtauthorization_reference_from_subscriptions.rb
@@ -1,6 +1,6 @@
 class RemoveRtauthorizationReferenceFromSubscriptions < ActiveRecord::Migration
   def change
     remove_reference :subscriptions, :rtauthorization, index: true
-    remove_foreign_key :subscriptionsbun, :rtauthorizations
+    remove_foreign_key :subscriptions, :rtauthorizations
   end
 end
