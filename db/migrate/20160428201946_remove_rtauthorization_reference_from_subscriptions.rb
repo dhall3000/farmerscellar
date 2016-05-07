e4fbe2e738ff3906b9fa69681a94042e08991b73
David Hall <david@farmerscellar.com>
2016-05-07 14:09:05 -0700

    heroku doesn't like remove_foreign_key i guess

--- db/migrate/20160428201946_remove_rtauthorization_reference_from_subscriptions.rb
+++ db/migrate/20160428201946_remove_rtauthorization_reference_from_subscriptions.rb
@@ -1,6 +1,6 @@
 class RemoveRtauthorizationReferenceFromSubscriptions < ActiveRecord::Migration
   def change
     remove_reference :subscriptions, :rtauthorization, index: true
-    remove_foreign_key :subscriptions, :rtauthorizations
+#    remove_foreign_key :subscriptions, :rtauthorizations
   end
 end
