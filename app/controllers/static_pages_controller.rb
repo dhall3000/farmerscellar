1f6a8a3901f64539dd6464b8b8af2659df7b6eff
David Hall <david@farmerscellar.com>
2016-07-06 15:23:37 -0700

    how things work and about rewrite along with opening up the htw page to non users and collapsing two landing page buttons in to one

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   before_action :redirect_to_root_if_user_not_admin, only: [:test_page, :test_exception]
-  before_action :logged_in_user, only: [:how_things_work]
-  before_action :redirect_to_root_if_user_lacks_access, only: [:how_things_work]
   
   def home
     @website_settings = WebsiteSetting.order("website_settings.id").last
