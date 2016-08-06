75b49b48bd9571287453d1726cbfca677d893717
David Hall <david@farmerscellar.com>
2016-08-06 10:53:12 -0700

    made dropsite help question mark bigger. switched to a 3 button model for user pickup page experience. now has 'open' 'close' and 'log out' buttons.

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,67 +1,78 @@
 class PickupsController < ApplicationController
 	before_action :redirect_to_root_if_user_not_dropsite_user
 
+  @@mockup_mode = false
+
   def new
     @is_dropsite_user = true
   end
 
   def create
-
-    mockup_mode = false
-
-    @is_dropsite_user = true
-  	entered_code = params[:pickup_code]
-  	@pickup_code = PickupCode.new(code: entered_code, user: current_user)
-
-  	if @pickup_code.valid? || mockup_mode
-  		@pickup_code = PickupCode.find_by(code: entered_code)  		
-  		if @pickup_code.nil? && !mockup_mode
-	  		flash.now[:danger] = "Invalid code entry"
-	  		render 'pickups/new'	  	
-  		else
-        if mockup_mode
-          @user = User.find_by(email: "c1@c.com")
-          @tote_items = get_fake_tote_items(@user)
-          create_fake_last_pickup(@user)
-        else
-          @user = @pickup_code.user
-          #get a product list of everything that's been delivered since the last pickup (or 7 days, whichever is more recent)
-          @tote_items = @user.tote_items_to_pickup                  
-        end
-
-        @last_pickup = @user.pickups.order("pickups.id").last
-  			#now create a new pickup to represent the current pickup  				 				
- 				@user.pickups.create
-
-        if Rails.env.production?
-          toggle_garage_door
-        end
-        
-        flash.now[:success] = "Thanks for checking out!"
-  		end
-  	else
-  		flash.now[:danger] = "Invalid code entry"
-  		render 'pickups/new'
-  	end
-
+    puts "PickupsController#create start"
+    display_user_data
+    puts "PickupsController#create end"
   end
 
-  def done
+  def toggle_garage_door
 
-    if Rails.env.production?
-      toggle_garage_door
+    puts "PickupsController.toggle_garage_door start"
+
+    if Rails.env.production?      
+            
+      url =  "http://#{request.ip}:1984/client?command=door2"
+      uri = URI(url)
+      response = Net::HTTP.get_response(uri)
+
+      puts "PickupsController.toggle_garage_door response: #{response.class.to_a}"
+      
     end
-    
-    redirect_to new_pickup_path
+
+    display_user_data
+    puts "PickupsController.toggle_garage_door end"    
 
   end
 
   private    
 
-    def toggle_garage_door
-      url =  "http://#{request.ip}:1984/client?command=door2"
-      uri = URI(url)
-      response = Net::HTTP.get(uri)
+    def display_user_data
+
+      @is_dropsite_user = true
+      @pickup_code = params[:pickup_code]
+      puts "PickupsController#create @pickup_code: #{@pickup_code.to_s}"
+      @pickup_code = PickupCode.new(code: @pickup_code, user: current_user)
+
+      if @pickup_code.valid? || @@mockup_mode
+        @pickup_code = PickupCode.find_by(code: @pickup_code.code)     
+        if @pickup_code.nil? && !@@mockup_mode
+          flash.now[:danger] = "Invalid code entry"
+          puts "PickupsController#create Invalid code entry: #{@pickup_code.to_s}"
+          render 'pickups/new'      
+        else
+
+          puts "PickupsController#create valid code. Now fetching user and tote items..."
+
+          if @@mockup_mode
+            @user = User.find_by(email: "c1@c.com")
+            @tote_items = get_fake_tote_items(@user)
+            create_fake_last_pickup(@user)
+          else
+            @user = @pickup_code.user
+            #get a product list of everything that's been delivered since the last pickup (or 7 days, whichever is more recent)
+            @tote_items = @user.tote_items_to_pickup                  
+          end
+
+          @last_pickup = @user.pickups.order("pickups.id").last
+          #now create a new pickup to represent the current pickup                  
+          @user.pickups.create
+          
+          render 'pickups/create'
+
+        end
+      else
+        flash.now[:danger] = "Invalid code entry"
+        render 'pickups/new'
+      end
+
     end
 
 	  def redirect_to_root_if_user_not_dropsite_user
