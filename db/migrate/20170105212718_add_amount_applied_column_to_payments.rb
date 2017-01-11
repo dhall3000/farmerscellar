618c3bf0d46ca4728d6bd48066d13d7d7e3de195
david <davideltonhall@gmail.com>
2017-01-11 12:39:07 -0800

    fixed migration adding new non nullable column to payments table

--- db/migrate/20170105212718_add_amount_applied_column_to_payments.rb
+++ db/migrate/20170105212718_add_amount_applied_column_to_payments.rb
@@ -1,5 +1,13 @@
 class AddAmountAppliedColumnToPayments < ActiveRecord::Migration[5.0]
   def change
-    add_column :payments, :amount_applied, :float, default: 0, null: false
+
+    add_column :payments, :amount_applied, :float, default: 0
+
+    Payment.all.each do |payment|
+      payment.update(amount_applied: payment.amount)
+    end
+
+    change_column :payments, :amount_applied, :float, null: false
+
   end
 end
