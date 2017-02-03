d9de530e470c0d83909e68aa579b22d136ed3331
david <davideltonhall@gmail.com>
2017-02-03 05:37:03 -0800

    put scheduled delivery date range up for posting range range selection

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -1,2 +1,24 @@
 module PostingsHelper
+
+  def get_delivery_range_schedule_text(postings)
+
+    if postings.nil? || !postings.any?
+      return nil
+    end
+
+    first = postings.order(:delivery_date).first.delivery_date
+    last =  postings.order(:delivery_date).last.delivery_date
+
+    if first == last
+      return "Delivery scheduled for #{friendly_date(first)}"
+    else
+      return "Deliveries scheduled #{friendly_date(first)} - #{friendly_date(last)}"
+    end
+
+  end
+
+  def friendly_date(date)
+    return "#{date.strftime("%a, %b")} #{date.day.ordinalize}"
+  end
+
 end
