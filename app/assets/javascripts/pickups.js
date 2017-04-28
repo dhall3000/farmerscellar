7deb87d2356388c12e69ead0d69a90d97f3d2ee4
david <davideltonhall@gmail.com>
2017-04-28 12:58:57 -0700

    now using jquery straight from kiosk -> garagedoorbuddy (via LAN) to cycle garage door. eliminated FC server from the process

--- app/assets/javascripts/pickups.js
+++ app/assets/javascripts/pickups.js
@@ -1,6 +1,12 @@
 $(function()
 {
 
+  $('#garageDoorButton').on('click', function () {
+    var $btn = $(this).button('loading')
+    $.get("http://10.0.0.19:1984/client?command=door2");
+    $btn.button('reset')
+  })
+
   $('#agree').on('click', function() {
     $('#pinForm').submit();
   });
